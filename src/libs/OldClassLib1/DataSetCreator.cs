396231cf4e9f2e56460a152c6fc387aef796fd88
Kristofer Nissbrandt <nissbran@gmail.com>
2017-09-05 23:39:12 +0200

    Added .NET 4.0 lib

---
+++ src/libs/OldClassLib1/DataSetCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace OldClassLib1
+{
+    public static class DataSetCreator
+    {
+        public static DataSet CreateDataSetFromFile(string filePath)
+        {
+            var dataSet = new DataSet();
+
+            dataSet.ReadXml(filePath);
+
+            return dataSet;
+        }
+    }
+}
---
+++ src/libs/OldConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using OldClassLib1;
+using System;
+
+namespace OldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataset = DataSetCreator.CreateDataSetFromFile("TestFile.xml");
+
+            Console.Write(dataset.GetXmlSchema());
+            Console.ReadLine();
+        }
+    }
+}
