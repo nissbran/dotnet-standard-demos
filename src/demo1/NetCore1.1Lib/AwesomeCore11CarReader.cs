7f3b6cc8f44ccca08b4b6899a27b6055a1320e11
Kristofer Nissbrandt <nissbran@gmail.com>
2017-09-12 18:25:38 +0200

    Added .net core lib

---
+++ src/demo1/NetCore1.1Lib/AwesomeCore11CarReader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace NetCore11Lib
+{
+    public class AwesomeCore11CarReader
+    {
+        public IEnumerable<string> GetCarNames(string xmlFileName)
+        {
+            var document = XDocument.Load(xmlFileName);
+
+            var carNames = document.Descendants(XName.Get("Name"));
+
+            return carNames.Select(element => element.Value + " Core 1.1");
+        }
+    }
+}
