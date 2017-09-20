32d7631d35e98c7f5fae8dba4cfcdefff0f3ec56
Kristofer Nissbrandt <nissbran@gmail.com>
2017-09-20 13:45:05 +0200

    Added C# 7.1

--- src/demo2/NetCore20ConsoleApp/Program.cs
+++ src/demo2/NetCore20ConsoleApp/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace NetCore20ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        // C# 7.1 async Main
+        public static async Task Main(string[] args)
         {
             Console.WriteLine("Hello from .NET Core 2.0");
 
+            var httpClient = new HttpClient();
 
+            var response = await httpClient.GetAsync("http://www.squeed.com");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var stream = await response.Content.ReadAsStreamAsync();
+
+                using (var reader = new StreamReader(stream))
+                {
+                    await reader.ReadLineAsync();
+                    var secondLine = await reader.ReadLineAsync();
+                    Console.WriteLine(secondLine);
+                }
+            }
+
+            
+            Console.ReadLine();
         }
     }
 }
